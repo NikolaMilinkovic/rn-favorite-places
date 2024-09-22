c108bdadda0016955e187a29ea37657b827926c4
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-09-22 21:16:43 +0200

    Add image preview

--- components/places/ImagePicker.js
+++ components/places/ImagePicker.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import { Button, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Button, Image, View, Text, StyleSheet } from 'react-native'
 import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker'
+import { Colors } from '../../constants/colors';
 
 function ImagePicker() {
+  const [previewImage, setPreviewImage] = useState('');
   const [cameraPermissionInformation, requestPermission] = useCameraPermissions();
   async function verifyPermissions(){
     if(cameraPermissionInformation.status === PermissionStatus.UNDETERMINED){
@@ -26,16 +28,39 @@ function ImagePicker() {
       aspect: [16,9],
       quality: 0.5
     });
-    console.log(image)
+    console.log(image.assets[0].uri)
+    setPreviewImage(image.assets[0])
+  }
+  
+  let imagePreview = <Text>No image taken yet.</Text>
+  if(previewImage){
+    imagePreview = <Image source={{uri: previewImage.uri}} style={styles.image}/>
   }
   return (
     <View>
-      <View>
-
+      <View style={styles.imagePreview}>
+        {imagePreview}
       </View>
       <Button title='Take image' onPress={takeImageHandler}/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    flex: 1,
+    width: '100%',
+    height: 300,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  }
+})
 
 export default ImagePicker
