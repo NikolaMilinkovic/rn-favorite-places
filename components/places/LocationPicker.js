4d8c2bcdc87d6adc60068e7bfc6ad3b2db1359fe
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-09-23 22:16:06 +0200

    Add Locate User & connect with Google Static Map API

--- components/places/LocationPicker.js
+++ components/places/LocationPicker.js
@@ -1,36 +1,55 @@
-import React from 'react'
-import { StyleSheet, View, Alert } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, View, Alert, Image, Text } from 'react-native'
 import OutlinedButton from '../ui/OutlinedButton'
 import { Colors } from '../../constants/colors'
 import { getCurrentPositionAsync, PermissionStatus, useForegroundPermissions } from 'expo-location'
+import { getMapPreview } from '../../util/location'
 
 function LocationPicker() {
   const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
+  const [pickedLocation, setPickedLocation] = useState();
 
   async function verifyPermissions(){
     if(locationPermissionInformation.status === PermissionStatus.UNDETERMINED){
       const permissionsResponse = await requestPermission();
+
       return permissionsResponse.granted;
     }
-    if(locationPermissionInformation.status = PermissionStatus.DENIED){
+    if(locationPermissionInformation.status === PermissionStatus.DENIED){
       Alert.alert('Insufficient Permissions', 'For this feature a permission to use location is neccessary.');
+
       return false;
     }
     return true;
   }
+
   async function getLocationHandler(){
     const hasPermission = await verifyPermissions();
     if(!hasPermission) return;
     const location = await getCurrentPositionAsync();
+    setPickedLocation({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    })
   }
   function pickOnMapHandler(){
 
   }
 
+  let locationPreview = <Text>No location picked yet.</Text>
+  if(pickedLocation){
+    locationPreview = (
+      <Image
+        style={styles.image} 
+        source={{ uri: getMapPreview(pickedLocation.lat, pickedLocation.lng) }} 
+      />
+    )
+  }
+
   return (
     <View>
       <View style={styles.mapPreview}>
-
+      {locationPreview}
       </View>
       <View style={styles.actions}>
         <OutlinedButton onPress={getLocationHandler} icon='location'>Locate User</OutlinedButton>
@@ -50,6 +69,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: Colors.primary100,
     borderRadius: 4,
+    overflow: 'hidden',
   },
   actions: {
     flexDirection: 'row',
@@ -57,6 +77,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom: 44,
   },
+  image: {
+    width: '100%',
+    height: '100%',
+  }
 })
 
 export default LocationPicker
